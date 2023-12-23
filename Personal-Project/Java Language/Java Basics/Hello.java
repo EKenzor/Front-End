29f9e3156f9e78c553dac646467dc8476fb1ad76
Villarino <94608907+EKenzor@users.noreply.github.com>
2023-12-23 16:52:24 +0800

    Add files via upload

---
+++ Personal-Project/Java Language/Java Basics/Hello.java
@@ -0,0 +1,7 @@
+/* First Java program, which says hello. */
+
+public class Hello {
+    public static void main(String[] args) { // Program entry
+        System.out.println("Hello, world"); // Print text message
+    }
+}
