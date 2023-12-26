1221c628ac3e6b0bd9844eded271d881e749dfa0
Villarino <94608907+EKenzor@users.noreply.github.com>
2023-12-26 22:10:59 +0800

    Delete Java Language directory

--- Java Language/Java Basics/Hello.java
+++
@@ -1,7 +0,0 @@
-/* First Java program, which says hello. */
-
-public class Hello {
-    public static void main(String[] args) { // Program entry
-        System.out.println("Hello, world"); // Print text message
-    }
-}
